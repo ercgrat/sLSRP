be14617aa869ed0dbd7638ff0e63b13d15d6058d
Eric Gratta <ercgrat@gmail.com>
2016-03-07 21:05:54 -0500

    Created Packet class and listening loop.

--- Router.java
+++ Router.java
@@ -1,11 +1,38 @@
 package sLSRP;
-
+import java.net.*;
 import java.util.*;
 
 public class Router {
 
+	public static volatile boolean failing = false;
+
 	public static void main(String args[]) {
+		// Create data structures
 		
+		// Read in configuration
+		
+		// Loop over neighbors in the configuration
+		
+		// Fork all threads
+		
+		// Create socket and listen
+		ServerSocket serverSocket = NetUtils.serverSocket();
+		while(true) {
+			if(!failing) {
+				System.out.println("Waiting to accept incoming client...");
+				SocketBundle client = NetUtils.acceptClient(serverSocket);
+				Packet packet = new Packet();
+				packet.readHeader(client);
+				
+				switch(packet.type) {
+					case Packet.Type.Alive:
+					case Packet.Type.Data:
+					case Packet.Type.LSA:
+					case Packet.Type.Neighbor:
+						break;
+				}
+			}
+		}
 	}
 
 }
