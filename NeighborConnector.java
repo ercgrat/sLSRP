96b124c814bf6d77517628f517ea65876c676fb5
youbehappy <youbehappy@qq.com>
2016-03-25 12:56:56 -0400

    Neighborhood Connection

--- NeighborConnector.java
+++ NeighborConnector.java
@@ -1,5 +1,7 @@
 package sLSRP;
 
+import java.io.IOException;
+
 
 public class NeighborConnector extends Thread {
 	Configuration config;
@@ -9,5 +11,16 @@ public class NeighborConnector extends Thread {
 	@Override
 	public void run() {
 		//TODO Iterate the neighbor list and establish connection with them.
+		SocketBundle client = NetUtils.clientSocket("", 1033);
+		int connectionType = 1;
+		try {
+			//Send the connection type
+			client.out.writeInt(connectionType);
+			//read response type
+			int responseType = client.in.readInt();
+			System.out.println("Build connection with neighbor, the response type is: "+responseType);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
