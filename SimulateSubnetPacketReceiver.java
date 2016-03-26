5db6e3e6c06114003a113ba26e91d2b400badccb
youbehappy <youbehappy@qq.com>
2016-03-26 12:19:17 -0400

    Simulate subnet packets sender and receiver(Separate programs)

---
+++ SimulateSubnetPacketReceiver.java
@@ -0,0 +1,24 @@
+package sLSRP;
+
+import java.io.Console;
+import java.util.Scanner;
+
+/*
+ * This class is used for testing packet or file transfer between routers;
+ * This is a seperate program that simulates accepting packets from the network.
+ */
+public class SimulateSubnetPacketReceiver {
+
+	
+//	public static void main(String[] args) {
+//		//TODO
+////		Scanner reader = new Scanner(System.in);
+////		System.out.println("Enter a file path: ");
+////		reader.nextInt();
+//		
+//		Console console = System.console();
+//		String s = console.readLine();
+//		String text = console.readLine();
+//	}
+
+}
---
+++ SimulateSubnetPacketSender.java
@@ -0,0 +1,24 @@
+package sLSRP;
+
+import java.io.Console;
+import java.util.Scanner;
+
+/*
+ * This class is used for testing packet or file transfer between routers;
+ * This is a seperate program that simulating injecting packets into the network, 
+ * then the network sends these packets to the subnet receiver.
+ */
+public class SimulateSubnetPacketSender {
+
+//	public static void main(String[] args) {
+//	//TODO
+////	Scanner reader = new Scanner(System.in);
+////	System.out.println("Enter a file path: ");
+////	reader.nextInt();
+//	
+//	Console console = System.console();
+//	String s = console.readLine();
+//	String text = console.readLine();
+//}
+	
+}
