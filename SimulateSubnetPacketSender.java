e937be74957b70ac802efb00fea8f8da1a0d1dd7
youbehappy <youbehappy@qq.com>
2016-04-03 15:53:17 -0400

    PacketSender

--- SimulateSubnetPacketSender.java
+++ SimulateSubnetPacketSender.java
@@ -1,6 +1,9 @@
 package sLSRP;
 
-import java.io.Console;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -9,16 +12,37 @@ import java.util.Scanner;
  * then the network sends these packets to the subnet receiver.
  */
 public class SimulateSubnetPacketSender {
-
+	int packetSize = 0;
+	public SimulateSubnetPacketSender(int packetSize){
+		this.packetSize = packetSize;
+	}
+	ArrayList<Byte> readFile(String filePath){
+        FileInputStream inputStream;
+        ArrayList<Byte> list = new ArrayList<Byte>();
+		try {
+			inputStream = new FileInputStream(filePath);
+	        int dataByte = 0;
+	        while((dataByte = inputStream.read()) != -1) {
+	        	list.add((byte)dataByte);
+	        }   
+	        inputStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		return list;
+	}
+	ArrayList<Packet> generatePackets(ArrayList<Byte> list){
+		ArrayList<Packet> packetList = new ArrayList<Packet>();
+		return packetList;
+	}
 //	public static void main(String[] args) {
-//	//TODO
-////	Scanner reader = new Scanner(System.in);
-////	System.out.println("Enter a file path: ");
-////	reader.nextInt();
-//	
-//	Console console = System.console();
-//	String s = console.readLine();
-//	String text = console.readLine();
-//}
-	
+//		//TODO
+//		Scanner reader = new Scanner(System.in);
+//		System.out.println("Enter a file path: ");
+//		String text = reader.nextLine();
+//		
+//		System.out.println("--->"+text);
+//	}
 }
