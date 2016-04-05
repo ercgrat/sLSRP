62c0093c3767d0b8d2735060d6adeba54c162af3
youbehappy <youbehappy@qq.com>
2016-04-05 14:11:33 -0400

    LSAGenerator

--- LSA.java
+++ LSA.java
@@ -7,6 +7,24 @@ import java.io.IOException;
 
 public class LSA {
 
+//	Router ID: Identifies the router sending the LSA
+//	LSA Type: Classifies the packet as of type LSA Advertising Router. It identifies the
+//	          originating router of the LSA. Knowing which router has originated a particular LSA
+//	          tells the calculating router whether the LSA should be used in the routing
+//	          calculation and, if so, how it should be used.
+//	LS Age:   Indicates the number of seconds since the LSA was originated. This field is
+//	          reset every time a new instance of the same LSA is received. If the age of the LSA
+//	          reaches a predefined threshold, ¡®¡®Age_Limit¡¯¡¯, the LSA is deleted from the database.
+//	LS Sequence Number: This field is used by the routers to distinguish between
+//	          instances of the same LSA. The LSA instance having the larger LS Sequence Number
+//	          is considered to be more recent.
+//	Length:   This field contains the length, in bytes, of the LSA counting both LSA header and contents.
+//	Number of Links: Identifies the number of links reported in the LSA.
+//	Link ID:    Identifies the ID of the Link.
+//	Link Data: Contains all data describing the status of the link based on the routing
+//	          metrics in terms of availability and cost. The cost metric, ranging from 1 to
+//	          Max_Cost, indicates the relative cost of sending data packets over the link, namely
+//	          the larger the cost, the less likely the data will be routed over the link.
 	public int router;
 	public Date age;
 	public int sequenceNumber;
--- LSAGenerator.java
+++ LSAGenerator.java
@@ -2,22 +2,36 @@ package sLSRP;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.ArrayList;
-
-public class LSAGenerator extends Thread {
+/**
+ * This class is singleton in the program so that every class in the program can access to this same instance.
+ * It it called to produce an LSA when some changes happen in the routing table
+ * (for example, loss a neighbor or gain a neighbor)
+ * It is also responsible for periodically sending LSAs.
+ */
+public class LSAGenerator {
+	private static LSAGenerator generator;
 	
+	private LSAGenerator() {}
+	
+	public static LSAGenerator getInstance(Configuration inConfig, NetworkInfo inNetInfo){
+		if(generator == null) {
+			generator = new LSAGenerator(inConfig,inNetInfo);
+		}
+		return generator;
+	}
 	int sequenceNumber;
+	Configuration config;
+	NetworkInfo netInfo;
 	
-	public LSAGenerator(Configuration inConfig, NetworkInfo inNetInfo) {
-		final Configuration config = inConfig;
-		final NetworkInfo netInfo = inNetInfo;
+	private LSAGenerator(Configuration inConfig, NetworkInfo inNetInfo) {
+		this.config = inConfig;
+		this.netInfo = inNetInfo;
 		sequenceNumber = 0;
 		
 		Timer timer = new Timer();  
 		timer.schedule(new TimerTask(){
 			public void run() {
-				sequenceNumber++;
-				LSA lsa = new LSA(config.routerID, sequenceNumber, netInfo.getNeighborLinks(config.routerID));
+				LSA lsa = generateLSA();
 				for(int i = 0; i < netInfo.getNeighbors().size(); i++) {
 					/*
 					 *
@@ -34,5 +48,9 @@ public class LSAGenerator extends Thread {
 			}
 		}, config.forwardInterval);
 	}
-	
+	public LSA generateLSA(){
+		sequenceNumber++;
+		LSA lsa = new LSA(config.routerID, sequenceNumber, netInfo.getNeighborLinks(config.routerID));
+		return lsa;
+	}
 }
