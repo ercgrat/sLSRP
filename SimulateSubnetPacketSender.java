30b45373258f8b9eca35da138baedd73b640c812
youbehappy <youbehappy@qq.com>
2016-04-13 15:03:07 -0400

    Edge Router

--- SimulateSubnetPacketSender.java
+++ SimulateSubnetPacketSender.java
@@ -15,10 +15,13 @@ import java.util.Scanner;
  */
 public class SimulateSubnetPacketSender {
     
-	int packetSize = 0;
+	static int packetLength = 0;
+	static int senderId;
+	static int destinationID; 
+	
     
-	public SimulateSubnetPacketSender(int packetSize){
-		this.packetSize = packetSize;
+	public SimulateSubnetPacketSender(int packetLength){
+		this.packetLength = packetLength;
 	}
     
 	static ArrayList<Byte> readFile(String filePath){
@@ -27,6 +30,7 @@ public class SimulateSubnetPacketSender {
 		try {
 			inputStream = new FileInputStream(filePath);
 	        int dataByte = 0;
+	        
 	        while((dataByte = inputStream.read()) != -1) {
 	        	list.add((byte)dataByte);
 	        }   
@@ -38,12 +42,47 @@ public class SimulateSubnetPacketSender {
 		} 
 		return list;
 	}
-    static void sendFile(String filePath){
+    static void sendFile(String filePath,String fileName){
     	ArrayList<Byte> list = readFile(filePath);
-    	generatePackets(list);
+    	generatePackets(list,fileName);
     }
-	static ArrayList<Packet> generatePackets(ArrayList<Byte> list){
+	static ArrayList<Packet> generatePackets(ArrayList<Byte> list,String fileName){
 		ArrayList<Packet> packetList = new ArrayList<Packet>();
+		int sequenceID = 1;
+		int fromIndex = 0;
+		int toIndex = fromIndex+packetLength;
+		boolean flag = true;
+		while(flag){
+			if(toIndex>=list.size()){
+				ArrayList<Byte> sublist = (ArrayList<Byte>) list.subList(fromIndex, list.size());
+				byte list2[] = new byte[sublist.size()];
+				for(int i=0;i<sublist.size();i++){
+					list2[i] = sublist.get(i);
+				}
+				try {
+					Packet p = new Packet(senderId,destinationID,sequenceID, true,list2);
+					packetList.add(p);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				flag = false;
+			}else{
+				ArrayList<Byte> sublist = (ArrayList<Byte>) list.subList(fromIndex, toIndex);
+				byte list2[] = new byte[sublist.size()];
+				for(int i=0;i<sublist.size();i++){
+					list2[i] = sublist.get(i);
+				}
+				try {
+					Packet p = new Packet(senderId,destinationID,sequenceID, true,list2);
+					packetList.add(p);
+					toIndex = fromIndex+packetLength;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
 		return packetList;
 	}
     
@@ -63,11 +102,13 @@ public class SimulateSubnetPacketSender {
 			System.out.println("Please enter a file name to be sent or enter 1 to send all the files.");
 			String name = reader.nextLine();
 			if("1".equals(name)){
-				
+				for(int i = 0;i<childFiles.length;i++){
+					sendFile(childFiles[i].getAbsolutePath(),childFiles[i].getName());
+				}
 			}else{
 				String fpath = filePath.getAbsolutePath()+File.separator+name;
 				if(new File(fpath).exists()){
-					sendFile(fpath);
+					sendFile(fpath,name);
 				}else{
 					System.out.println("The file does not exist.");
 					userAction();
