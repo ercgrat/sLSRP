6783124e07a971bb200e2036ffa00b4c82c70e44
youbehappy <youbehappy@qq.com>
2016-04-13 13:54:28 -0400

    edge router

--- SimulateSubnetPacketSender.java
+++ SimulateSubnetPacketSender.java
@@ -1,6 +1,7 @@
 package sLSRP;
 
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,7 +21,7 @@ public class SimulateSubnetPacketSender {
 		this.packetSize = packetSize;
 	}
     
-	ArrayList<Byte> readFile(String filePath){
+	static ArrayList<Byte> readFile(String filePath){
         FileInputStream inputStream;
         ArrayList<Byte> list = new ArrayList<Byte>();
 		try {
@@ -37,8 +38,11 @@ public class SimulateSubnetPacketSender {
 		} 
 		return list;
 	}
-    
-	ArrayList<Packet> generatePackets(ArrayList<Byte> list){
+    static void sendFile(String filePath){
+    	ArrayList<Byte> list = readFile(filePath);
+    	generatePackets(list);
+    }
+	static ArrayList<Packet> generatePackets(ArrayList<Byte> list){
 		ArrayList<Packet> packetList = new ArrayList<Packet>();
 		return packetList;
 	}
@@ -48,15 +52,26 @@ public class SimulateSubnetPacketSender {
 		System.out.println("Please enter a directory path.");
 		String text = reader.nextLine();
 		System.out.println("Your input is: "+text);
-		
+		System.out.println("\n");
 		File filePath = new File(text);
 		if(filePath.exists() && filePath.isDirectory()){
+			File[] childFiles = filePath.listFiles();
+			for(int i = 0;i<childFiles.length;i++){
+				System.out.println(childFiles[i].getName());
+			}
+			System.out.println("\n");
 			System.out.println("Please enter a file name to be sent or enter 1 to send all the files.");
 			String name = reader.nextLine();
 			if("1".equals(name)){
 				
 			}else{
-				
+				String fpath = filePath.getAbsolutePath()+File.separator+name;
+				if(new File(fpath).exists()){
+					sendFile(fpath);
+				}else{
+					System.out.println("The file does not exist.");
+					userAction();
+				}
 			}
 		}else{
 			System.out.println("Please enter a valid directory path.");
